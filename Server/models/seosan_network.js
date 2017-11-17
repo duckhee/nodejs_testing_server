0e6be9a922dcb2085cc5d75203de591f336037e8
duckheewon <fain9301@yahoo.com>
2017-11-17 11:10:30 +0900

    fix unique sn_serial

--- Server/models/seosan_network.js
+++ Server/models/seosan_network.js
@@ -1,15 +1,18 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var seosan_network = sequelize.define('seosan_network', {
-    sn_serial: DataTypes.STRING,
-    sn_address: DataTypes.STRING,
-    sn_type: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
-  return seosan_network;
+    var seosan_network = sequelize.define('seosan_network', {
+        sn_serial: {
+            type: DataTypes.STRING,
+            unique: true,
+        },
+        sn_address: DataTypes.STRING,
+        sn_type: DataTypes.STRING
+    }, {
+        classMethods: {
+            associate: function(models) {
+                // associations can be defined here
+            }
+        }
+    });
+    return seosan_network;
 };
