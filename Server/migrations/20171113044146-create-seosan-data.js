930b74c056615b3742fd3ba555f351b3efb4f2ee
duckheewon <fain9301@yahoo.com>
2017-11-24 15:53:27 +0900

    fix models

--- Server/migrations/20171113044146-create-seosan-data.js
+++ Server/migrations/20171113044146-create-seosan-data.js
@@ -1,33 +1,34 @@
 'use strict';
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('seosan_data', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      sd_address: {
-        type: Sequelize.STRING
-      },
-      sd_serial: {
-        type: Sequelize.STRING
-      },
-      sd_data: {
-        type: Sequelize.TEXT
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('seosan_data');
-  }
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.createTable('seosan_data', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            sd_address: {
+                type: Sequelize.STRING
+            },
+            sd_serial: {
+                type: Sequelize.STRING
+            },
+            sd_data: {
+                type: Sequelize.TEXT
+            },
+            createdAt: {
+                allowNull: false,
+                type: 'TIMESTAMP',
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.dropTable('seosan_data');
+    }
 };
