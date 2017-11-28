2376b789d1622c4ef3c8c3f3634f4bf53203daf4
duckheewon <fain9301@yahoo.com>
2017-11-28 20:12:51 +0900

    test

--- app.js
+++ app.js
@@ -167,9 +167,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser({
-    secret: 'secret my key',
-}));
+app.use(cookieParser({}));
 app.use(session({
     secret: 'secret my key',
     resave: false,
