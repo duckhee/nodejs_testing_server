ddcbd47b0047e62a4ec79ef1ccccce23090723e1
woo417 <woo417@nate.com>
2017-11-28 19:43:52 +0900

    소켓 확인

--- Server/controllers/data_controller.js
+++ Server/controllers/data_controller.js
@@ -32,17 +32,6 @@ exports.insert_data = function(data_info, callback) {
     })
 };
 
-//insert array data callback(row, err);
-exports.insert_array_data = function(data_info, callback) {
-    models.seosan_data.bulkCreate([
-        data_info
-      ]).then(() => { // Notice: There are no arguments here, as of right now you'll have to...
-        //return models.seosan_data.findAll();
-      }).then(seosan_data => {
-        console.log(models.seosan_data) // ... in order to get the array of user objects
-      })
-};
-
 //list data all callback(rows, err);
 exports.list_data = function(data_info, callback) {
     models.seosan_data.findAll({
