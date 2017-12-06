17fca1afb299ac458dbc8a7c6afe10f18d89b10f
Ubuntu <ubuntu@ip-10-0-1-197.ap-northeast-2.compute.internal>
2017-12-06 09:53:46 +0900

    server backup

--- Server/util/file.js
+++ Server/util/file.js
@@ -287,7 +287,7 @@ exports.file_csv = function(file_info, callback) {
             console.log('success ! ');
 
             //make csv file or download file 
-            csvfile.write('download_date ' + date + '.csv', file_info.response);
+            csvfile.write('download_date ' + date + '.xlsx', file_info.response);
             //만약 서버에 파일을 생성을 하고 싶으면 밑에 주석 풀고 위에 주석 처리
             //csvfile.write('download_date'+date+'.csv);
             callback(row, null);
